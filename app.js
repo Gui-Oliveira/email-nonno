b59f0b7707e573247374e4df2bd4d1515a4c7dd9
Guilherme Silva <g.oliiveira@hotmail.com>
2023-07-29 21:40:50 -0300

    update attachments

--- app.js
+++ app.js
@@ -1,51 +1,59 @@
-const express = require('express');
-const nodemailer = require('nodemailer');
+const express = require("express");
+const nodemailer = require("nodemailer");
 const app = express();
 const PORT = 3000;
-const cors = require('cors');
-const multer = require('multer');
-const path = require('path');
+const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
 
 app.use(express.json());
 app.use(cors());
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, 'anexos'));
+    cb(null, "anexos");
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
-  }
+  },
 });
 
 const upload = multer({ storage: storage });
 
-
-app.post('/enviar-email', upload.array('anexos', 10), (req, res) => {
+app.post("/enviar-email", upload.array("anexos", 10), (req, res) => {
   const { nome, email, telefone, assunto, mensagem } = req.body;
+  const anexos = req.files.map((file) => ({
+    filename: file.originalname,
+    path: file.path,
+  }));
 
   const transporter = nodemailer.createTransport({
-    service: 'Outlook',
+    service: "Outlook",
     auth: {
-      user: 'g.oliiveira@hotmail.com',
-      pass: '60fcky0ur53lf'
-    }
+      user: "g.oliiveira@hotmail.com",
+      pass: "60fcky0ur53lf",
+    },
   });
 
   const mailOptions = {
-    from: 'g.oliiveira@hotmail.com',
-    to: 'contato_nonno@libero.it',
-    subject: 'Novo contato da página',
-    text: `Nome: ${nome}\nE-mail: ${email}\nTelefone: ${telefone}\nAssunto: ${assunto}\nMensagem: ${mensagem}`
+    from: "g.oliiveira@hotmail.com",
+    to: "g.oliiveira@hotmail.com",
+    // to: "contato_nonno@libero.it",
+    subject: "Novo contato da página",
+    text: `Nome: ${nome}\nE-mail: ${email}\nTelefone: ${telefone}\nAssunto: ${assunto}\nMensagem: ${mensagem}`,
+    attachments: anexos.map((anexo) => ({
+      filename: anexo.filename,
+      path: anexo.path,
+    })),
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.log(error);
-      res.status(500).send({ message: 'Erro ao enviar e-mail', error });
+      res.status(500).send({ message: "Erro ao enviar e-mail", error });
     } else {
-      console.log('E-mail enviado: ' + info.response);
-      res.status(200).send({ message: 'E-mail enviado com sucesso' });
+      console.log("E-mail enviado: " + info.response);
+      res.status(200).send({ message: "E-mail enviado com sucesso" });
     }
   });
 });
