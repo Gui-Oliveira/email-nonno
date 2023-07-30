2b9bd7b9ab45ce61bb741d2a8b11e61ab534d6fb
Guilherme Silva <g.oliiveira@hotmail.com>
2023-07-30 10:40:14 -0300

    Update app.js

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.post("/enviar-email", upload.array("anexos", 10), (req, res) => {
   }));
 
   const transporter = nodemailer.createTransport({
-    service: "Gmail",
+    service: "Libero",
     auth: {
       user: `${process.env.NODEMAILER_USERNAME}`,
       pass: `${process.env.NODEMAILER_PASSWORD}`,
